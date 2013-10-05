34d80cc50b05f36b697300dbc9a2f82a032cc4cf
Ernesto <ernesto>
2013-10-05 11:30:18 +0200

    New MacApp Target

---
+++ Examples/Idiomatic/IdiomaticMac/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  IdiomaticMac
+//
+//  Created by Ernesto on 10/5/13.
+//  Copyright (c) 2013 The Mental Faculty B.V. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Examples/Idiomatic/IdiomaticMacTests/IdiomaticMacTests.m
@@ -0,0 +1,34 @@
+//
+//  IdiomaticMacTests.m
+//  IdiomaticMacTests
+//
+//  Created by Ernesto on 10/5/13.
+//  Copyright (c) 2013 The Mental Faculty B.V. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface IdiomaticMacTests : XCTestCase
+
+@end
+
+@implementation IdiomaticMacTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
