03ddadf7256eb10d6834790a1be7863679e5c316
Ernesto <ernesto>
2013-10-05 11:42:06 +0200

    Initial Mac App setup

--- Examples/Idiomatic/IdiomaticMac/AppDelegate.m
+++ Examples/Idiomatic/IdiomaticMac/AppDelegate.m
@@ -7,12 +7,104 @@
 //
 
 #import "AppDelegate.h"
+#import <CoreData/CoreData.h>
+
+#import "CoreDataEnsembles.h"
+#import "CDEICloudFileSystem.h"
+
+NSString * const IDMSyncActivityDidBeginNotification = @"IDMSyncActivityDidBegin";
+NSString * const IDMSyncActivityDidEndNotification = @"IDMSyncActivityDidEnd";
+
+@interface AppDelegate () <CDEPersistentStoreEnsembleDelegate>
+{
+    NSManagedObjectContext *managedObjectContext;
+    CDEPersistentStoreEnsemble *ensemble;
+    CDEICloudFileSystem *cloudFileSystem;
+    NSUInteger activeMergeCount;
+
+}
+@end
+
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    // Ensembles logging
+    CDESetCurrentLoggingLevel(CDELoggingLevelWarning);
+    
+    // Store directory and URL
+    NSURL *directoryURL = [[NSFileManager defaultManager] URLForDirectory:NSApplicationSupportDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:YES error:NULL];
+    directoryURL = [directoryURL URLByAppendingPathComponent:NSBundle.mainBundle.bundleIdentifier isDirectory:YES];
+    [[NSFileManager defaultManager] createDirectoryAtURL:directoryURL withIntermediateDirectories:YES attributes:nil error:NULL];
+    NSURL *storeURL = [directoryURL URLByAppendingPathComponent:@"store.sqlite"];
+    
+    // Setup Core Data Stack
+    NSError *error;
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Model" withExtension:@"momd"];
+    NSManagedObjectModel *model = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    NSPersistentStoreCoordinator *coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:model];
+    [coordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error];
+    managedObjectContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
+    managedObjectContext.persistentStoreCoordinator = coordinator;
+    managedObjectContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy;
+    
+    // Setup Ensemble
+    cloudFileSystem = [[CDEICloudFileSystem alloc] initWithUbiquityContainerIdentifier:@"P7BXV6PHLD.com.mentalfaculty.idiomatic"];
+    ensemble = [[CDEPersistentStoreEnsemble alloc] initWithEnsembleIdentifier:@"MainStore" persistentStorePath:storeURL.path managedObjectModel:model cloudFileSystem:cloudFileSystem];
+    ensemble.delegate = self;
+    
+
 }
+
+#pragma mark - Sync Methods
+
+- (void)decrementMergeCount
+{
+    activeMergeCount--;
+    if (activeMergeCount == 0) {
+        [[NSNotificationCenter defaultCenter] postNotificationName:IDMSyncActivityDidEndNotification object:nil];
+    }
+}
+
+- (void)incrementMergeCount
+{
+    activeMergeCount++;
+    if (activeMergeCount == 1) {
+        [[NSNotificationCenter defaultCenter] postNotificationName:IDMSyncActivityDidBeginNotification object:nil];
+    }
+}
+
+- (void)synchronize
+{
+    [self incrementMergeCount];
+    if (!ensemble.isLeeched) {
+        [ensemble leechPersistentStoreWithCompletion:^(NSError *error) {
+            [self decrementMergeCount];
+            if (error) NSLog(@"Could not leech to ensemble: %@", error);
+        }];
+    }
+    else {
+        [ensemble mergeWithCompletion:^(NSError *error) {
+            [self decrementMergeCount];
+            if (error) NSLog(@"Error merging: %@", error);
+        }];
+    }
+}
+
+#pragma mark - Persistent Store Ensemble Delegate
+
+- (void)persistentStoreEnsemble:(CDEPersistentStoreEnsemble *)ensemble didSaveMergeChangesWithNotification:(NSNotification *)notification
+{
+    [managedObjectContext performBlock:^{
+        [managedObjectContext mergeChangesFromContextDidSaveNotification:notification];
+    }];
+}
+
+- (NSArray *)persistentStoreEnsemble:(CDEPersistentStoreEnsemble *)ensemble globalIdentifiersForManagedObjects:(NSArray *)objects
+{
+    return [objects valueForKeyPath:@"uniqueIdentifier"];
+}
+
 
 @end
