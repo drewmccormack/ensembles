c01602f72b59dbb1cbf5e162396ce75cece1373e
Drew McCormack <drewmccormack@mac.com>
2013-12-08 19:41:44 +0100

    Added more unit tests for the CDEBaselineConsolidator. One is failing.

--- Framework/Tests/CDEBaselineConsolidatorTests.m
+++ Framework/Tests/CDEBaselineConsolidatorTests.m
@@ -10,8 +10,10 @@
 #import "CDEEventStoreTestCase.h"
 #import "CDEBaselineConsolidator.h"
 #import "CDEStoreModificationEvent.h"
+#import "CDEGlobalIdentifier.h"
 #import "CDEEventRevision.h"
 #import "CDERevisionSet.h"
+#import "CDEPropertyChangeValue.h"
 #import "CDERevision.h"
 
 @interface CDEBaselineConsolidatorTests : CDEEventStoreTestCase
@@ -59,7 +61,7 @@
     XCTAssertTrue([consolidator baselineNeedsConsolidation], @"Should need to consolidate two baselines");
 }
 
-- (void)testConsolidatingMultipleBaselinesLeavesMostRecent
+- (void)testConsolidatingMultipleBaselinesKeepsMostRecent
 {
     [self addBaselineEventsForStoreId:@"123" globalCounts:@[@(2), @(0), @(1)] revisions:@[@(2), @(0), @(1)]];
     
@@ -79,7 +81,7 @@
     [self waitForAsyncOpToFinish];
 }
 
-- (void)testConsolidatingMultipleBaselinesOnWithVariousStoresLeavesMostRecent
+- (void)testConsolidatingMultipleBaselinesWithMultipleStoresKeepsMostRecent
 {
     [self addBaselineEventsForStoreId:@"123" globalCounts:@[@(2), @(0), @(1)] revisions:@[@(2), @(0), @(1)]];
     [self addBaselineEventsForStoreId:@"234" globalCounts:@[@(3)] revisions:@[@(0)]];
@@ -91,7 +93,7 @@
             
             CDEStoreModificationEvent *event = events.lastObject;
             XCTAssertEqual(event.globalCount, (int64_t)3, @"Wrong event was kept");
-            XCTAssertEqualObjects(event.eventRevision.persistentStoreIdentifier, @"store1", @"Store id should be the event store id");
+            XCTAssertEqualObjects(event.eventRevision.persistentStoreIdentifier, self.eventStore.persistentStoreIdentifier, @"Store id should be the event store id");
 
             dispatch_async(dispatch_get_main_queue(), ^{
                 [self stopAsyncOp];
@@ -101,8 +103,120 @@
     [self waitForAsyncOpToFinish];
 }
 
-- (void)addBaselineEventsForStoreId:(NSString *)storeId globalCounts:(NSArray *)globalCounts revisions:(NSArray *)revisions
+- (void)testMergingConcurrentBaselinesKeepsMostRecentObjectChange
 {
+    CDEStoreModificationEvent *baseline0 = [[self addBaselineEventsForStoreId:@"123" globalCounts:@[@(10)] revisions:@[@(10)]] lastObject];
+    CDEStoreModificationEvent *baseline1 = [[self addBaselineEventsForStoreId:@"234" globalCounts:@[@(20)] revisions:@[@(10)]] lastObject];
+    [context performBlockAndWait:^{
+        CDEGlobalIdentifier *globalId = [NSEntityDescription insertNewObjectForEntityForName:@"CDEGlobalIdentifier" inManagedObjectContext:context];
+        globalId.globalIdentifier = @"123";
+        globalId.nameOfEntity = @"Parent";
+
+        CDEObjectChange *change1 = [self objectChangeForGlobalId:globalId valuesByKey:@{@"date":[NSDate dateWithTimeIntervalSince1970:10]}];
+        change1.storeModificationEvent = baseline0;
+        
+        CDEObjectChange *change2 = [self objectChangeForGlobalId:globalId valuesByKey:@{@"date":[NSDate dateWithTimeIntervalSince1970:20]}];
+        change2.storeModificationEvent = baseline1;
+        
+        NSError *error;
+        XCTAssertTrue([context save:&error], @"Failed to save");
+    }];
+    
+    
+    [consolidator consolidateBaselineWithCompletion:^(NSError *error) {
+        [context performBlock:^{
+            NSArray *events = [self storeModEvents];
+            CDEStoreModificationEvent *event = events.lastObject;
+            
+            NSSet *changes = event.objectChanges;
+            XCTAssertEqual(changes.count, (NSUInteger)1, @"Wrong number of changes");
+            
+            CDEObjectChange *change = changes.anyObject;
+            XCTAssertEqual(change.type, CDEObjectChangeTypeInsert, @"Wrong type");
+            
+            NSArray *values = change.propertyChangeValues;
+            XCTAssertEqual(values.count, (NSUInteger)1, @"Wrong number of values");
+            
+            CDEPropertyChangeValue *value = values.lastObject;
+            XCTAssertEqualObjects(value.value, [NSDate dateWithTimeIntervalSince1970:20], @"Wrong value");
+            XCTAssertEqual(value.type, CDEPropertyChangeTypeAttribute, @"Wrong type");
+
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self stopAsyncOp];
+            });
+        }];
+    }];
+    [self waitForAsyncOpToFinish];
+}
+
+- (void)testMergingConcurrentBaselinesMergesPropertyValues
+{
+    CDEStoreModificationEvent *baseline0 = [[self addBaselineEventsForStoreId:@"123" globalCounts:@[@(10)] revisions:@[@(10)]] lastObject];
+    CDEStoreModificationEvent *baseline1 = [[self addBaselineEventsForStoreId:@"234" globalCounts:@[@(20)] revisions:@[@(10)]] lastObject];
+    [context performBlockAndWait:^{
+        CDEGlobalIdentifier *globalId = [NSEntityDescription insertNewObjectForEntityForName:@"CDEGlobalIdentifier" inManagedObjectContext:context];
+        globalId.globalIdentifier = @"123";
+        globalId.nameOfEntity = @"Parent";
+        
+        NSDictionary *values = @{@"date":[NSDate dateWithTimeIntervalSince1970:10], @"strength":@5};
+        CDEObjectChange *change1 = [self objectChangeForGlobalId:globalId valuesByKey:values];
+        change1.storeModificationEvent = baseline0;
+        
+        values = @{@"date":[NSDate dateWithTimeIntervalSince1970:20]};
+        CDEObjectChange *change2 = [self objectChangeForGlobalId:globalId valuesByKey:values];
+        change2.storeModificationEvent = baseline1;
+        
+        NSError *error;
+        XCTAssertTrue([context save:&error], @"Failed to save");
+    }];
+    
+    
+    [consolidator consolidateBaselineWithCompletion:^(NSError *error) {
+        [context performBlock:^{
+            NSArray *events = [self storeModEvents];
+            CDEStoreModificationEvent *event = events.lastObject;
+            
+            NSSet *changes = event.objectChanges;
+            XCTAssertEqual(changes .count, (NSUInteger)1, @"Wrong number of object changes");
+            
+            CDEObjectChange *change = changes.anyObject;
+            NSArray *values = change.propertyChangeValues;
+            XCTAssertEqual(values.count, (NSUInteger)2, @"Wrong number of values");
+            
+            NSArray *filteredValues = [values filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"propertyName = 'strength'"]];
+            CDEPropertyChangeValue *value = filteredValues.lastObject;
+            XCTAssertEqualObjects(value.value, @5, @"Wrong value");
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self stopAsyncOp];
+            });
+        }];
+    }];
+    [self waitForAsyncOpToFinish];
+}
+
+- (CDEObjectChange *)objectChangeForGlobalId:(CDEGlobalIdentifier *)globalId valuesByKey:(NSDictionary *)valuesByKey
+{
+    CDEObjectChange *change = [NSEntityDescription insertNewObjectForEntityForName:@"CDEObjectChange" inManagedObjectContext:context];
+    change.type = CDEObjectChangeTypeInsert;
+    change.globalIdentifier = globalId;
+    change.nameOfEntity = globalId.nameOfEntity;
+    
+    NSMutableArray *values = [NSMutableArray array];
+    for (NSString *key in valuesByKey) {
+        CDEPropertyChangeValue *value = [[CDEPropertyChangeValue alloc] initWithType:CDEPropertyChangeTypeAttribute propertyName:@"date"];
+        value.value = valuesByKey[key];
+        [values addObject:value];
+    }
+
+    change.propertyChangeValues = values;
+    
+    return change;
+}
+
+- (NSArray *)addBaselineEventsForStoreId:(NSString *)storeId globalCounts:(NSArray *)globalCounts revisions:(NSArray *)revisions
+{
+    __block NSMutableArray *baselines = [NSMutableArray array];
     [context performBlockAndWait:^{
         for (NSUInteger i = 0; i < globalCounts.count; i++) {
             CDEStoreModificationEvent *event = [NSEntityDescription insertNewObjectForEntityForName:@"CDEStoreModificationEvent" inManagedObjectContext:context];
@@ -113,10 +227,14 @@
             CDEEventRevision *rev;
             rev = [CDEEventRevision makeEventRevisionForPersistentStoreIdentifier:storeId revisionNumber:[revisions[i] integerValue] inManagedObjectContext:context];
             event.eventRevision = rev;
+            
+            [baselines addObject:event];
         }
         
         [context save:NULL];
     }];
+    
+    return baselines;
 }
 
 - (void)waitForAsyncOpToFinish
