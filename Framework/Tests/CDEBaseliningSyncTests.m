70f0378c936e313eb37a5c4d0b77b5867883822a
Drew McCormack <drewmccormack@mac.com>
2014-01-27 14:36:08 +0100

    Started adding baselining tests. Not all passing at this point.

--- Framework/Tests/CDEBaseliningSyncTests.m
+++ Framework/Tests/CDEBaseliningSyncTests.m
@@ -6,29 +6,125 @@
 //  Copyright (c) 2014 Drew McCormack. All rights reserved.
 //
 
-#import <XCTest/XCTest.h>
+#import "CDESyncTest.h"
+#import "CDEEventStore.h"
+#import "CDEPersistentStoreEnsemble.h"
 
-@interface CDEBaseliningSyncTests : XCTestCase
+@interface CDEBaseliningSyncTests : CDESyncTest
 
 @end
 
-@implementation CDEBaseliningSyncTests
+@implementation CDEBaseliningSyncTests {
+    NSString *cloudBaselinesDir, *cloudEventsDir;
+}
 
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    cloudBaselinesDir = [cloudRootDir stringByAppendingPathComponent:@"com.ensembles.synctest/baselines"];
+    cloudEventsDir = [cloudRootDir stringByAppendingPathComponent:@"com.ensembles.synctest/events"];
 }
 
-- (void)tearDown
+- (void)testCloudBaselineUniquenessWithNoInitialData
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+    [self leechStores];
+    XCTAssertNil([self syncChanges], @"Sync failed");
+    NSArray *baselineFiles = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:cloudBaselinesDir error:NULL];
+    XCTAssertEqual(baselineFiles.count, (NSUInteger)1, @"Should only be one baseline");
+    
+    NSArray *eventFiles = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:cloudEventsDir error:NULL];
+    XCTAssertEqual(eventFiles.count, (NSUInteger)0, @"Should only be one baseline");
 }
 
-- (void)testExample
+- (void)testCloudBaselineFileContentDuringLeechAndSync
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    NSManagedObject *parentOnDevice1 = [NSEntityDescription insertNewObjectForEntityForName:@"Parent" inManagedObjectContext:context1];
+    [parentOnDevice1 setValue:@"bob" forKey:@"name"];
+    XCTAssertTrue([context1 save:NULL], @"Could not save");
+    
+    // Leech and merge first store
+    [ensemble1 leechPersistentStoreWithCompletion:^(NSError *error) {
+        XCTAssertNil(error, @"Error leeching first store");
+        [self completeAsync];
+    }];
+    [self waitForAsync];
+    [self mergeEnsemble:ensemble1];
+    
+    // Check cloud files and contents
+    NSArray *baselineFiles = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:cloudBaselinesDir error:NULL];
+    NSString *baseline1 = baselineFiles.lastObject;
+    NSString *baseline1Path = [cloudBaselinesDir stringByAppendingPathComponent:baseline1];
+    XCTAssertEqual(baselineFiles.count, (NSUInteger)1, @"Should be a baseline");
+    
+    NSArray *events = [self fetchEventsInEventFile:baseline1Path];
+    XCTAssertEqual(events.count, (NSUInteger)1, @"Should be a parent object in baseline file after the first context first merges");
+    
+    // Leech second store
+    [ensemble2 leechPersistentStoreWithCompletion:^(NSError *error) {
+        XCTAssertNil(error, @"Error leeching second store");
+        [self completeAsync];
+    }];
+    [self waitForAsync];
+    
+    // Check cloud file is unchanged
+    events = [self fetchEventsInEventFile:baseline1Path];
+    XCTAssertEqual(events.count, (NSUInteger)1, @"Should be a parent object in baseline before store two merges");
+    
+    // Merge second store. Should consolidate baselines.
+    [self mergeEnsemble:ensemble2];
+    
+    events = [self fetchEventsInEventFile:baseline1Path];
+    XCTAssertEqual(events.count, (NSUInteger)0, @"After second store merges, first baseline should not exist");
+    
+    // Check new baseline file
+    baselineFiles = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:cloudBaselinesDir error:NULL];
+    NSString *baseline2 = baselineFiles.lastObject;
+    NSString *baseline2Path = [cloudBaselinesDir stringByAppendingPathComponent:baseline2];
+    XCTAssertEqual(baselineFiles.count, (NSUInteger)1, @"Should be a baseline");
+    XCTAssertNotEqualObjects(baseline1, baseline2, @"Baselines not have same file name");
+    
+    // Check content
+    events = [self fetchEventsInEventFile:baseline2Path];
+    XCTAssertEqual(events.count, (NSUInteger)1, @"Should have one baseline");
+    
+    // Check things are still OK after several merges
+    [self syncChanges];
+    baselineFiles = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:cloudBaselinesDir error:NULL];
+    XCTAssertEqual(baselineFiles.count, (NSUInteger)1, @"Should be a baseline");
+}
+
+- (void)testBaselineConsolidation
+{
+    NSManagedObject *parentOnDevice1 = [NSEntityDescription insertNewObjectForEntityForName:@"Parent" inManagedObjectContext:context1];
+    [parentOnDevice1 setValue:@"bob" forKey:@"name"];
+    XCTAssertTrue([context1 save:NULL], @"Could not save");
+    
+    [self leechStores];
+    [self syncChanges];
+    
+    NSFetchRequest *fetch = [NSFetchRequest fetchRequestWithEntityName:@"Parent"];
+    NSArray *parents = [context2 executeFetchRequest:fetch error:NULL];
+    XCTAssertEqual(parents.count, (NSUInteger)1, @"Should be a parent object in context2");
+}
+
+- (NSManagedObjectContext *)eventFileContextForURL:(NSURL *)baselineURL
+{
+    NSManagedObjectContext *baselineContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSConfinementConcurrencyType];
+    NSURL *modelURL = [[NSBundle bundleForClass:[CDEEventStore class]] URLForResource:@"CDEEventStoreModel" withExtension:@"momd"];
+    NSManagedObjectModel *eventModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    NSPersistentStoreCoordinator *coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:eventModel];
+    baselineContext.persistentStoreCoordinator = coordinator;
+    NSPersistentStore *store = [coordinator addPersistentStoreWithType:NSBinaryStoreType configuration:nil URL:baselineURL options:nil error:NULL];
+    XCTAssertNotNil(store, @"Store was nil");
+    return baselineContext;
+}
+
+- (NSArray *)fetchEventsInEventFile:(NSString *)path
+{
+    NSManagedObjectContext *context = [self eventFileContextForURL:[NSURL fileURLWithPath:path]];
+    NSFetchRequest *fetch = [NSFetchRequest fetchRequestWithEntityName:@"CDEStoreModificationEvent"];
+    NSArray *events = [context executeFetchRequest:fetch error:NULL];
+    return events;
 }
 
 @end
