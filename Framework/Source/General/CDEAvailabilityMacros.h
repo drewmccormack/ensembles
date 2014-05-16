29cbb151fb4c42e9909a3a50a5e481b4fb6bb866
Drew McCormack <drewmccormack@mac.com>
2014-05-16 11:10:52 +0200

    Added foundation import to availability macros.

--- Framework/Source/General/CDEAvailabilityMacros.h
+++ Framework/Source/General/CDEAvailabilityMacros.h
@@ -1,7 +1,9 @@
+#import <Foundation/Foundation.h>
+
 #ifndef __IPHONE_7_0
-    #define __IPHONE_7_0     70000
+    #define __IPHONE_7_0 70000
 #endif
 
 #ifndef __MAC_10_9
-    #define __MAC_10_9      1090
+    #define __MAC_10_9 1090
 #endif
